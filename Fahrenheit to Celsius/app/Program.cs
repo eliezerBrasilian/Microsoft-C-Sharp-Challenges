a27136dfbdd73398d3d5ee83479e35094ee3a33d
Eliezer Assuncao <eliezerassuncaocustodio@gmail.com>
2023-08-23 00:03:48 -0300

    fahreheit to celsius

---
+++ Fahrenheit to Celsius/app/Program.cs
@@ -0,0 +1,7 @@
+﻿int fahrenheit = 94;
+
+decimal fahrenheitToCelsius = (fahrenheit - 32)* (5/(decimal)9);
+
+int leadingDigit = (int)fahrenheitToCelsius;
+int trailingDigits = (int)(fahrenheitToCelsius * 100) - ( leadingDigit * 100);
+Console.WriteLine($"The temperature is {leadingDigit}.{trailingDigits} Celsius.");
